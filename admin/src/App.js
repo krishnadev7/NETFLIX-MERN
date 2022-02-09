d5e3449f244880cce26e72ed955fca716d590fdc
krishnadev v <krishnadevv12@gmail.comgit config --global user.name krishnadev7>
2022-02-09 12:59:16 +0530

    admin jwt login commited

--- admin/src/App.js
+++ admin/src/App.js
@@ -2,47 +2,60 @@ import './App.css';
 import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
 import UserList from './pages/userList/UserList';
 import User from './pages/user/User';
 import NewUser from './pages/newUser/NewUser';
 import ProductList from './pages/productList/ProductList';
 import Product from './pages/product/Product';
 import NewProduct from './pages/newProduct/NewProduct';
-import { useEffect, useMemo, useState } from 'react';
+import { Fragment, useContext, useEffect, useMemo, useState } from 'react';
 import axios from 'axios';
+import Login from './pages/login/Login';
+import { AuthContext } from './contest/authContext/AuthContext';
 
 function App() {
-  
+  const { user } = useContext(AuthContext);
+
   return (
     <Router>
-      <Topbar />
-      <div className='container'>
-        <Sidebar />
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/users'>
-            <UserList />
-          </Route>
-          <Route path='/user/:userId'>
-            <User />
-          </Route>
-          <Route path='/newUser'>
-            <NewUser />
-          </Route>
-          <Route path='/movies'>
-            <ProductList />
-          </Route>
-          <Route path='/product/:productsId'>
-            <Product />
-          </Route>
-          <Route path='/newproduct'>
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route path='/login'>{user ? <Redirect to='/' /> : <Login />}</Route>
+        {user && (
+          <Fragment>
+            <Topbar />
+            <div className='container'>
+              <Sidebar />
+              <Route exact path='/'>
+                <Home />
+              </Route>
+              <Route path='/users'>
+                <UserList />
+              </Route>
+              <Route path='/user/:userId'>
+                <User />
+              </Route>
+              <Route path='/newUser'>
+                <NewUser />
+              </Route>
+              <Route path='/movies'>
+                <ProductList />
+              </Route>
+              <Route path='/product/:productsId'>
+                <Product />
+              </Route>
+              <Route path='/newproduct'>
+                <NewProduct />
+              </Route>
+            </div>
+          </Fragment>
+        )}
+      </Switch>
     </Router>
   );
 }
